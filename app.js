a48b1347990e9f5a2262bc4c580c4c618ab245a5
BrunoF2P <brunobispof2p@gmail.com>
2024-05-01 07:47:05 -0300

    adicionando login,registro e categorias

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require("express");
+const passport = require("./api/utils/passport-configs");
 
-var indexRouter = require('./routes/indexRoutes');
+const indexRouter = require('./api/routes/indexRoutes');
+const registerRoutes = require('./api/routes/registerRoutes');
+const loginRoutes = require('./api/routes/loginRoutes');
+const categoryRoutes = require('./api/routes/categoryRoutes');
 
-var app = express();
+const app = express();
+
+app.use(bodyParser.json());
+app.use(passport.initialize());
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.use('/', indexRouter);
+app.use(indexRouter);
+app.use(registerRoutes)
+app.use(loginRoutes)
+app.use(categoryRoutes)
 
 
 module.exports = app;
