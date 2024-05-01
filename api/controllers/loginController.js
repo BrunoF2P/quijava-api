dc56d3f49eff8422fd4e2342d400da73c5ea3f22
BrunoF2P <brunobispof2p@gmail.com>
2024-05-01 07:49:40 -0300

    opcao de login com jwt

---
+++ api/controllers/loginController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const sequelize = require("../config/db.js");
+const initModels = require("../models/init-models");
+const jwtConfig = require("../config/jwtConfig");
+const {validateInput} = require("../utils/validationUtils");
+
+const models = initModels(sequelize);
+
+const loginUser = async (req, res, next) => {
+    const { username, password } = req.body;
+
+    // Valida os campos de entrada
+    const validation = validateInput(username, password);
+    if (!validation.valid) {
+        return res.status(400).json({ message: validation.message });
+    }
+
+    try {
+        // Verifica se o usuário existe no banco de dados
+        const user = await models.users.findOne({ where: { username } });
+        if (!user) {
+            return res.status(401).json({ message: 'Usuário não encontrado' });
+        }
+
+        // Verifica se a senha está correta
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Senha incorreta' });
+        }
+
+        // Gera o token JWT
+        const payload = { username: user.username.trim(), id: user.user_id };
+
+        const token = jwt.sign(payload, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn, algorithm: jwtConfig.algorithm });
+
+        // Retorna o token
+        return res.status(200).json({ token });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+module.exports = { loginUser };
