d840a10dcd98d1b0252aac142aa203e8cffc6581
BrunoF2P <brunobispof2p@gmail.com>
2024-05-01 02:20:39 -0300

    config de inicializacao e teste de conexcao com a db

---
+++ api/config/db.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require('sequelize');
+
+require('dotenv').config()
+
+const sequelize = new Sequelize(
+    process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: 'postgres'
+});
+
+module.exports = sequelize;
